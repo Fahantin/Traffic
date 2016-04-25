6aeddeb92b3059295a3154dfccde2059f03c5b73
Fahantin <thiago_fantin@hotmail.com>
2016-04-25 18:11:42 -0300

    MyLocation, DAO, Model, Inicio do Geofence, Metodos para última location.

---
+++ app/src/main/java/kmk/gotraffic/controller/util/GeofenceController.java
@@ -0,0 +1,28 @@
+//package kmk.gotraffic.controller.util;
+//
+//import android.content.Context;
+//import android.content.SharedPreferences;
+//
+//import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.location.Geofence;
+//import com.google.gson.Gson;
+//
+//public class GeofenceController {
+//
+//    private final String TAG = GeofenceController.class.getName();
+//
+//    private Context context;
+//    private GoogleApiClient googleApiClient;
+//    private Gson gson;
+//    private SharedPreferences prefs;
+//
+//    private List<NamedGeofence> namedGeofences;
+//    public List<NamedGeofence> getNamedGeofences() {
+//        return namedGeofences;
+//    }
+//
+//    private List<NamedGeofence> namedGeofencesToRemove;
+//
+//    private Geofence geofenceToAdd;
+//    private NamedGeofence namedGeofenceToAdd;
+//}
